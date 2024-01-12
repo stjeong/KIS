c3040c34b7a9791e8f7a5e011e4c93787eafda2d
SeongTae Jeong <techsharer@outlook.com>
2024-01-12 19:06:59 +0900

    주식매입 시 "GW라우팅 중 오류가 발생했습니다." 오류 해결

---
+++ eFriendOpenAPI/Extension/HttpClientExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Json;
+
+namespace eFriendOpenAPI.Extension;
+
+public static class HttpClientExtension
+{
+    public static async Task<HttpResponseMessage> PostJsonContent(this HttpClient client, string url, object content)
+    {
+        var jsonContent = JsonContent.Create(content);
+        await jsonContent.LoadIntoBufferAsync();
+        return await client.PostAsync(url, jsonContent);
+    }
+}
